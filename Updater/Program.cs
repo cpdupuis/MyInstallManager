63e6368f179cbf519cb6974899ea8f9f716c0d72
Duncan McIntosh <dmcintosh@mozilla.com>
2026-07-17 12:51:42 -0400

    Provide a separate Updater executable
    Same as the others, just with no resources.

---
+++ Updater/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyInstallManager;
+
+using System.Reflection;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        return await InstallManager.Main(args, Assembly.GetExecutingAssembly());
+    }
+}
