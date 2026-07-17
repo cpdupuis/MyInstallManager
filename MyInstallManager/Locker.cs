5210878b9fd96b56c323ad84e848b2b80f5279ee
Duncan McIntosh <dmcintosh@mozilla.com>
2026-07-17 12:25:43 -0400

    Use LockFile/UnlockFile directly inside of Locker
    It turns out that File.Lock refuses to lock if another process already
    holds the lock, so use the underlying system APIs directly to block.
    
    (It is misleading that WithLock/WithoutLock are async despite blocking the
    event loop as they wait; plausibly in the final version this should use
    overlapped I/O instead.)

--- MyInstallManager/Locker.cs
+++ MyInstallManager/Locker.cs
@@ -1,20 +1,82 @@
 namespace MyInstallManager;
 
-public class Locker
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+public class Locker : IDisposable
 {
-    private Locker()
+    [DllImport("kernel32.dll")]
+    static extern bool LockFileEx(SafeFileHandle hFile, uint dwFlags, uint dwReserved,
+                                  uint nNumberOfBytesToLockLow, uint nNumberOfBytesToLockHigh,
+                                  [In] ref System.Threading.NativeOverlapped lpOverlapped);
+    [DllImport("kernel32.dll")]
+    static extern bool UnlockFileEx(SafeFileHandle hFile, uint dwReserved,
+                                    uint nNumberOfBytesToUnlockLow, uint nNumberOfBytesToUnlockHigh,
+                                    [In] ref System.Threading.NativeOverlapped lpOverlapped);
+
+    private FileStream fs;
+    private SafeFileHandle handle;
+
+    private Locker(string lockname)
     {
-        // Don't instantiate me.
+        this.fs = File.Open(lockname, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read | FileShare.Write | FileShare.Delete);
+        this.handle = fs.SafeFileHandle;
     }
 
-    public static async Task WithLock(string lockname, Func<Task> action)
+    public void Dispose() {
+        handle.Dispose();
+        fs.Dispose();
+    }
+
+    public static async Task WithLock(string lockname, Func<Locker, Task> action)
     {
-        using (FileStream fs = File.Open(lockname, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read | FileShare.Write | FileShare.Delete)) {
-            // C# implicitly takes an exclusive lock, which is conveniently
-            // exactly what we want.
-            fs.Lock(0, 1);
-            await action();
+        using (Locker locker = new Locker(lockname)) {
+            await locker.WithLock(action);
         }
     }
 
+    public async Task WithLock(Func<Locker, Task> action) {
+        Acquire();
+        try {
+            await action(this);
+        } finally {
+            Release();
+        }
+    }
+
+    public async Task WithoutLock(Func<Locker, Task> action) {
+        Release();
+        try {
+            await action(this);
+        } finally {
+            Acquire();
+        }
+    }
+
+    private void Acquire() {
+        System.Threading.NativeOverlapped overlapped = new();
+        overlapped.EventHandle = 0;
+
+        LockFileEx(
+            handle,
+            2, // LOCKFILE_EXCLUSIVE_LOCK
+            0,
+            1,
+            0,
+            ref overlapped
+        );
+    }
+
+    private void Release() {
+        System.Threading.NativeOverlapped overlapped = new();
+        overlapped.EventHandle = 0;
+
+        UnlockFileEx(
+            handle,
+            0,
+            1,
+            0,
+            ref overlapped
+        );
+    }
 }
